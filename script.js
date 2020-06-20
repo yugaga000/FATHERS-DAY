3da888e5d3623279aa961ebcf224039845d2eab3
yugaga000 <64200402+yugaga000@users.noreply.github.com>
2020-06-20 18:11:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,34 @@ function fade(){
     TweenMax.to("#surprise",1,{
         opacity:1,
     })
+    TweenMax.to("#three",1,{
+        top:"-100%",
+        delay:3,
+        ease : Power4.easeInOut
+    })
+    TweenMax.to("#quote",1,{
+        top:"-100%",
+        delay:4,
+        ease : Power4.easeInOut
+    })
+    TweenMax.to("#quote2",1,{
+        top:"-100%",
+        delay:9,
+        ease : Power4.easeInOut
+    })
+    TweenMax.to("#quote3",1,{
+        top:"-100%",
+        delay:14,
+        ease : Power4.easeInOut
+    })
+    TweenMax.to("#quote4",1,{
+        top:"-100%",
+        delay:19,
+        ease : Power4.easeInOut
+    })
+    TweenMax.to("#quote5",1,{
+        top:"-100%",
+        delay:24,
+        ease : Power4.easeInOut
+    })
 }
